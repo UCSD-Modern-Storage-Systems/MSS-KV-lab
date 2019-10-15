cd758026f13d5a9702726771c718bb81fd87689e
Juno Kim <juno@eng.ucsd.edu>
2019-10-15 14:00:51 -0700

    Port test cases from pmemkv

--- test/basic_test.cc
+++ test/basic_test.cc
@@ -25,32 +25,40 @@ public:
 	}
 
 	status get(string_view key, std::string *value) {
-		void *v = pmkv_get(_kv, key.data(), key.size());
-		if (!v)
+		char val[MAX_VAL_LEN];
+		size_t val_size;
+		int s = pmkv_get(_kv, key.data(), key.size(), val, &val_size);
+		if (s)
 			return status::NOT_FOUND;
-		*value = std::string((char*)v);
+		value->assign(val, val_size);
 		return status::OK;
 	}
 
 	status put(string_view key, string_view value) {
-		pmkv_put(_kv, key.data(), key.size(), value.data(), value.size());
+		int s = pmkv_put(_kv, key.data(), key.size(), value.data(), value.size());
+		if (s)
+			throw std::runtime_error("Failed to put with an undefined error");
 		return status::OK;
 	}
 
 	status remove(string_view key) {
-		pmkv_del(_kv, key.data(), key.size());
+		int s = pmkv_del(_kv, key.data(), key.size());
+		if (s)
+			return status::NOT_FOUND;
 		return status::OK;
 	}
 
 	status count_all(std::size_t &cnt) {
-		pmkv_count_all(_kv, &cnt);
+		int s = pmkv_count_all(_kv, &cnt);
+		if (s)
+			throw std::runtime_error("Failed to count all with an undefined error");
 		return status::OK;
 	}
 
 	status exists(string_view key) {
-		if (pmkv_exists(_kv, key.data(), key.size()))
-			return status::OK;
-		return status::NOT_FOUND;
+		if (!pmkv_exists(_kv, key.data(), key.size()))
+			return status::NOT_FOUND;
+		return status::OK;
 	}
 
 private:
@@ -151,6 +159,371 @@ TEST_F(PMKVTest, BinaryKeyTest)
 	ASSERT_TRUE(kv->get(key1, &value3) == status::NOT_FOUND);
 	ASSERT_TRUE(kv->get("a", &value3) == status::OK && value3 == "should_not_change");
 }
+
+TEST_F(PMKVTest, BinaryValueTest)
+{
+	std::string value("A\0B\0\0C", 6);
+	ASSERT_TRUE(kv->put("key1", value) == status::OK) << errormsg();
+	std::string value_out;
+	ASSERT_TRUE(kv->get("key1", &value_out) == status::OK &&
+		    (value_out.length() == 6) && (value_out == value));
+}
+
+TEST_F(PMKVTest, EmptyKeyTest)
+{
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(kv->put("", "empty") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->put(" ", "single-space") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 2);
+	ASSERT_TRUE(kv->put("\t\t", "two-tab") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 3);
+	std::string value1;
+	std::string value2;
+	std::string value3;
+	ASSERT_TRUE(status::OK == kv->exists(""));
+	ASSERT_TRUE(kv->get("", &value1) == status::OK && value1 == "empty");
+	ASSERT_TRUE(status::OK == kv->exists(" "));
+	ASSERT_TRUE(kv->get(" ", &value2) == status::OK && value2 == "single-space");
+	ASSERT_TRUE(status::OK == kv->exists("\t\t"));
+	ASSERT_TRUE(kv->get("\t\t", &value3) == status::OK && value3 == "two-tab");
+}
+
+TEST_F(PMKVTest, EmptyValueTest)
+{
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(kv->put("empty", "") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->put("single-space", " ") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 2);
+	ASSERT_TRUE(kv->put("two-tab", "\t\t") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 3);
+	std::string value1;
+	std::string value2;
+	std::string value3;
+	ASSERT_TRUE(kv->get("empty", &value1) == status::OK && value1 == "");
+	ASSERT_TRUE(kv->get("single-space", &value2) == status::OK && value2 == " ");
+	ASSERT_TRUE(kv->get("two-tab", &value3) == status::OK && value3 == "\t\t");
+}
+
+TEST_F(PMKVTest, GetClearExternalValueTest)
+{
+	ASSERT_TRUE(kv->put("key1", "cool") == status::OK) << errormsg();
+	std::string value = "super";
+	ASSERT_TRUE(kv->get("key1", &value) == status::OK && value == "cool");
+
+	value = "super";
+	ASSERT_TRUE(kv->get("non_existent_key", &value) == status::NOT_FOUND &&
+		    value == "super");
+}
+
+TEST_F(PMKVTest, GetHeadlessTest)
+{
+	ASSERT_TRUE(status::NOT_FOUND == kv->exists("waldo"));
+	std::string value;
+	ASSERT_TRUE(kv->get("waldo", &value) == status::NOT_FOUND);
+}
+
+TEST_F(PMKVTest, GetMultipleTest)
+{
+	ASSERT_TRUE(kv->put("abc", "A1") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->put("def", "B2") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->put("hij", "C3") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->put("jkl", "D4") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->put("mno", "E5") == status::OK) << errormsg();
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 5);
+	ASSERT_TRUE(status::OK == kv->exists("abc"));
+	std::string value1;
+	ASSERT_TRUE(kv->get("abc", &value1) == status::OK && value1 == "A1");
+	ASSERT_TRUE(status::OK == kv->exists("def"));
+	std::string value2;
+	ASSERT_TRUE(kv->get("def", &value2) == status::OK && value2 == "B2");
+	ASSERT_TRUE(status::OK == kv->exists("hij"));
+	std::string value3;
+	ASSERT_TRUE(kv->get("hij", &value3) == status::OK && value3 == "C3");
+	ASSERT_TRUE(status::OK == kv->exists("jkl"));
+	std::string value4;
+	ASSERT_TRUE(kv->get("jkl", &value4) == status::OK && value4 == "D4");
+	ASSERT_TRUE(status::OK == kv->exists("mno"));
+	std::string value5;
+	ASSERT_TRUE(kv->get("mno", &value5) == status::OK && value5 == "E5");
+}
+
+TEST_F(PMKVTest, GetMultiple2Test)
+{
+	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->put("key2", "value2") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->put("key3", "value3") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->remove("key2") == status::OK);
+	ASSERT_TRUE(kv->put("key3", "VALUE3") == status::OK) << errormsg();
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 2);
+	std::string value1;
+	ASSERT_TRUE(kv->get("key1", &value1) == status::OK && value1 == "value1");
+	std::string value2;
+	ASSERT_TRUE(kv->get("key2", &value2) == status::NOT_FOUND);
+	std::string value3;
+	ASSERT_TRUE(kv->get("key3", &value3) == status::OK && value3 == "VALUE3");
+}
+
+TEST_F(PMKVTest, GetNonexistentTest)
+{
+	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << errormsg();
+	ASSERT_TRUE(status::NOT_FOUND == kv->exists("waldo"));
+	std::string value;
+	ASSERT_TRUE(kv->get("waldo", &value) == status::NOT_FOUND);
+}
+
+TEST_F(PMKVTest, PutTest)
+{
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+
+	std::string value;
+	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->get("key1", &value) == status::OK && value == "value1");
+
+	std::string new_value;
+	ASSERT_TRUE(kv->put("key1", "VALUE1") == status::OK) << errormsg(); // same size
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->get("key1", &new_value) == status::OK && new_value == "VALUE1");
+
+	std::string new_value2;
+	ASSERT_TRUE(kv->put("key1", "new_value") == status::OK)
+		<< errormsg(); // longer size
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->get("key1", &new_value2) == status::OK &&
+		    new_value2 == "new_value");
+
+	std::string new_value3;
+	ASSERT_TRUE(kv->put("key1", "?") == status::OK) << errormsg(); // shorter size
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->get("key1", &new_value3) == status::OK && new_value3 == "?");
+}
+
+TEST_F(PMKVTest, PutKeysOfDifferentSizesTest)
+{
+	std::string value;
+	ASSERT_TRUE(kv->put("123456789ABCDE", "A") == status::OK) << errormsg();
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->get("123456789ABCDE", &value) == status::OK && value == "A");
+
+	std::string value2;
+	ASSERT_TRUE(kv->put("123456789ABCDEF", "B") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 2);
+	ASSERT_TRUE(kv->get("123456789ABCDEF", &value2) == status::OK && value2 == "B");
+
+	std::string value3;
+	ASSERT_TRUE(kv->put("12345678ABCDEFG", "C") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 3);
+	ASSERT_TRUE(kv->get("12345678ABCDEFG", &value3) == status::OK && value3 == "C");
+
+	std::string value4;
+	ASSERT_TRUE(kv->put("123456789", "D") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 4);
+	ASSERT_TRUE(kv->get("123456789", &value4) == status::OK && value4 == "D");
+
+	std::string value5;
+	ASSERT_TRUE(kv->put("123456789ABCDEFGHI", "E") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 5);
+	ASSERT_TRUE(kv->get("123456789ABCDEFGHI", &value5) == status::OK &&
+		    value5 == "E");
+}
+
+TEST_F(PMKVTest, PutValuesOfDifferentSizesTest)
+{
+	std::string value;
+	ASSERT_TRUE(kv->put("A", "123456789ABCDE") == status::OK) << errormsg();
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->get("A", &value) == status::OK && value == "123456789ABCDE");
+
+	std::string value2;
+	ASSERT_TRUE(kv->put("B", "123456789ABCDEF") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 2);
+	ASSERT_TRUE(kv->get("B", &value2) == status::OK && value2 == "123456789ABCDEF");
+
+	std::string value3;
+	ASSERT_TRUE(kv->put("C", "12345678ABCDEFG") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 3);
+	ASSERT_TRUE(kv->get("C", &value3) == status::OK && value3 == "12345678ABCDEFG");
+
+	std::string value4;
+	ASSERT_TRUE(kv->put("D", "123456789") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 4);
+	ASSERT_TRUE(kv->get("D", &value4) == status::OK && value4 == "123456789");
+
+	std::string value5;
+	ASSERT_TRUE(kv->put("E", "123456789ABCDEFGHI") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 5);
+	ASSERT_TRUE(kv->get("E", &value5) == status::OK &&
+		    value5 == "123456789ABCDEFGHI");
+}
+
+TEST_F(PMKVTest, RemoveAllTest)
+{
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(kv->put("tmpkey", "tmpvalue1") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->remove("tmpkey") == status::OK);
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(status::NOT_FOUND == kv->exists("tmpkey"));
+	std::string value;
+	ASSERT_TRUE(kv->get("tmpkey", &value) == status::NOT_FOUND);
+}
+
+TEST_F(PMKVTest, RemoveAndInsertTest)
+{
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(kv->put("tmpkey", "tmpvalue1") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->remove("tmpkey") == status::OK);
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(status::NOT_FOUND == kv->exists("tmpkey"));
+	std::string value;
+	ASSERT_TRUE(kv->get("tmpkey", &value) == status::NOT_FOUND);
+	ASSERT_TRUE(kv->put("tmpkey1", "tmpvalue1") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(status::OK == kv->exists("tmpkey1"));
+	ASSERT_TRUE(kv->get("tmpkey1", &value) == status::OK && value == "tmpvalue1");
+	ASSERT_TRUE(kv->remove("tmpkey1") == status::OK);
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(status::NOT_FOUND == kv->exists("tmpkey1"));
+	ASSERT_TRUE(kv->get("tmpkey1", &value) == status::NOT_FOUND);
+}
+
+TEST_F(PMKVTest, RemoveExistingTest)
+{
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 0);
+	ASSERT_TRUE(kv->put("tmpkey1", "tmpvalue1") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->put("tmpkey2", "tmpvalue2") == status::OK) << errormsg();
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 2);
+	ASSERT_TRUE(kv->remove("tmpkey1") == status::OK);
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(kv->remove("tmpkey1") == status::NOT_FOUND);
+	cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == 1);
+	ASSERT_TRUE(status::NOT_FOUND == kv->exists("tmpkey1"));
+	std::string value;
+	ASSERT_TRUE(kv->get("tmpkey1", &value) == status::NOT_FOUND);
+	ASSERT_TRUE(status::OK == kv->exists("tmpkey2"));
+	ASSERT_TRUE(kv->get("tmpkey2", &value) == status::OK && value == "tmpvalue2");
+}
+
+TEST_F(PMKVTest, RemoveHeadlessTest)
+{
+	ASSERT_TRUE(kv->remove("nada") == status::NOT_FOUND);
+}
+
+TEST_F(PMKVTest, RemoveNonexistentTest)
+{
+	ASSERT_TRUE(kv->put("key1", "value1") == status::OK) << errormsg();
+	ASSERT_TRUE(kv->remove("nada") == status::NOT_FOUND);
+	ASSERT_TRUE(status::OK == kv->exists("key1"));
+}
+
+/*
+TEST_F(PMKVTest, SimpleMultithreadedTest)
+{
+	size_t threads_number = 8;
+	size_t thread_items = 50;
+	parallel_exec(threads_number, [&](size_t thread_id) {
+		size_t begin = thread_id * thread_items;
+		size_t end = begin + thread_items;
+		for (auto i = begin; i < end; i++) {
+			std::string istr = std::to_string(i);
+			ASSERT_TRUE(kv->put(istr, (istr + "!")) == status::OK)
+				<< errormsg();
+			std::string value;
+			ASSERT_TRUE(kv->get(istr, &value) == status::OK &&
+				    value == (istr + "!"));
+		}
+		for (auto i = begin; i < end; i++) {
+			std::string istr = std::to_string(i);
+			std::string value;
+			ASSERT_TRUE(kv->get(istr, &value) == status::OK &&
+				    value == (istr + "!"));
+		}
+	});
+	std::size_t cnt = std::numeric_limits<std::size_t>::max();
+	ASSERT_TRUE(kv->count_all(cnt) == status::OK);
+	ASSERT_TRUE(cnt == threads_number * thread_items);
+}
+*/
 
 
 int main(int argc, char **argv) {
