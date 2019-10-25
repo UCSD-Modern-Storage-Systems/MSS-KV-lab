c382b1b2aad3073eaf23122efb3c621b74652d5e
Juno Kim <juk146@eng.ucsd.edu>
2019-10-25 01:08:26 -0700

    Add config, lab.py for autograder purpose

---
+++ lab.py
@@ -0,0 +1,21 @@
+# Students should not edit this file, since changes here will _only_
+# affect how your code runs locally.  It will not change how your code
+# executes in the cloud.
+ 
+lab_name = "PMKV"
+output_files = []
+input_files = ['src/pmkv.c']
+run_cmd = ['make', 'test_all']
+clean_cmd = ['make', 'clean']
+repo = "https://github.com/UCSD-Modern-Storage-Systems/MSS-KV-lab"
+reference_tag = "314bfbd09ab3a28b446742234851eeef2c29dcba"
+time_limit = 600
+
+valid_options={
+}
+
+default_options={
+}
+
+figures_of_merit=[
+]
